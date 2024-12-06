c8c429b1f16d52aa4ce3bffab80c985bac9327e4
Javier Palacios <javijpa@hotmail.com>
2024-12-06 07:21:35 +0100

    Rewrote day 6 part 2 to use arrays instead of hashmaps

--- day06_2.cpp
+++ day06_2.cpp
@@ -42,8 +42,12 @@ int main()
 	enum {up, right,down, left};
 	s_v2i offsets[] = {v2i(0, -1), v2i(1, 0), v2i(0, 1), v2i(-1, 0)};
 	int base_guard_dir = up;
-	s_hashmap<s_v2i, b8, 20480> base_obstacle_map = zero;
-	s_hashmap<s_v2i, int, 20480> base_visit_map = zero;
+	b8 base_obstacle_arr[200][200] = zero;
+	struct s_dir {
+		b8 valid;
+		int dir;
+	};
+	s_dir base_visit_arr[200][200] = zero;
 	int steps = 0;
 
 	for(int line_i = 0; line_i < line_count; line_i += 1) {
@@ -57,7 +61,7 @@ int main()
 		while(*line) {
 			int x = (int)(line - base_line);
 			if(*line == '#') {
-				hashmap_set(&base_obstacle_map, v2i(x, line_i), true);
+				base_obstacle_arr[line_i][x] = true;
 				line += 1;
 			}
 			else if(*line == '^') {
@@ -89,9 +93,10 @@ int main()
 	{
 		s_v2i guard_pos = base_guard_pos;
 		int guard_dir = base_guard_dir;
-		hashmap_set(&base_visit_map, guard_pos, guard_dir);
+		base_visit_arr[guard_pos.y][guard_pos.x] = {true, guard_dir};
 		while(true) {
-			if(hashmap_get(&base_obstacle_map, guard_pos + offsets[guard_dir])) {
+			s_v2i next_pos = guard_pos + offsets[guard_dir];
+			if(is_valid_index(next_pos, line_len, line_count) && base_obstacle_arr[next_pos.y][next_pos.x]) {
 				guard_dir = (guard_dir + 1) % 4;
 				steps += 1;
 			}
@@ -101,7 +106,7 @@ int main()
 					break;
 				}
 				else {
-					hashmap_set(&base_visit_map, guard_pos, guard_dir);
+					base_visit_arr[guard_pos.y][guard_pos.x] = {true, guard_dir};
 				}
 			}
 		}
@@ -111,37 +116,39 @@ int main()
 	for(int y = 0; y < line_count; y += 1) {
 		for(int x = 0; x < line_len; x += 1) {
 			s_v2i index = v2i(x, y);
-			if(!hashmap_get(&base_obstacle_map, v2i(x, y)) && v2i(x, y) != base_guard_pos) {
-				printf("%i, %i\n", x, y);
-				if(!hashmap_get(&base_visit_map, index)) { continue; }
+			if(!base_obstacle_arr[y][x] && v2i(x, y) != base_guard_pos) {
+				// printf("%i, %i\n", x, y);
+				if(!base_visit_arr[index.y][index.x].valid) { continue; };
 
-				s_hashmap<s_v2i, b8, 20480> obstacle_map = base_obstacle_map;
+				b8 obstacle_arr[200][200];
+				memcpy(obstacle_arr, base_obstacle_arr, sizeof(obstacle_arr));
 				s_v2i guard_pos = base_guard_pos;
 				int guard_dir = base_guard_dir;
 				b8 in_a_loop = false;
-				s_hashmap<s_v2i, int, 20480> visit_map = zero;
-				hashmap_set(&visit_map, guard_pos, guard_dir);
-				hashmap_set(&obstacle_map, v2i(x, y), true);
 
+				s_dir visit_arr[200][200] = zero;
+				visit_arr[guard_pos.y][guard_pos.x] = {true, guard_dir};
+				obstacle_arr[y][x] = true;
 
 				while(true) {
-					if(hashmap_get(&obstacle_map, guard_pos + offsets[guard_dir])) {
+					s_v2i next_pos = guard_pos + offsets[guard_dir];
+					if(is_valid_index(next_pos, line_len, line_count) && obstacle_arr[next_pos.y][next_pos.x]) {
 						guard_dir = (guard_dir + 1) % 4;
 						steps += 1;
 					}
 					else {
-						guard_pos = guard_pos + offsets[guard_dir];
+						guard_pos = next_pos;
 						if(!is_valid_index(guard_pos, line_len, line_count)) {
 							break;
 						}
 						else {
-							int* prev_dir = hashmap_get(&visit_map, guard_pos);
-							if(prev_dir && *prev_dir == guard_dir) {
+							s_dir prev_dir = visit_arr[guard_pos.y][guard_pos.x];
+							if(prev_dir.valid && prev_dir.dir == guard_dir) {
 								in_a_loop = true;
 								loop_count += 1;
 								break;
 							}
-							hashmap_set(&visit_map, guard_pos, guard_dir);
+							visit_arr[guard_pos.y][guard_pos.x] = {true, guard_dir};
 						}
 					}
 				}
