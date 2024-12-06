2f5ab21e6c2a8a3abbdd51cedf6bbf4d75ba9fe0
Javier Palacios <javijpa@hotmail.com>
2024-12-06 07:25:46 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ cls
 setlocal
 
 set target=day06_2.cpp
-set compile=-nologo -Fe:main.exe -I..\my_libs2 -Zi -O2 -wd4505 -D_CRT_SECURE_NO_WARNINGS -W3
+set compile=-nologo -Fe:main.exe -I..\my_libs2 -Zi -Od -wd4505 -D_CRT_SECURE_NO_WARNINGS -W3
 set linker=-link /STACK:0x4000000,0x4000000
 
 if not "%1"=="" set target=%1
